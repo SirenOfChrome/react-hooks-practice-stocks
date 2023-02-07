5045eca5bcf9927c065f672c336ecec763ca616c
Forrest Jones <forrestjones2010@gmail.com>
2023-02-07 13:47:33 -0600

    GDC#4

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,15 +1,48 @@
 import React from "react";
-import Stock from "./Stock";
 
-function PortfolioContainer() {
+function SearchBar({ sortBy, onChangeSort, filterBy, onChangeFilter }) {
+  function handleSortChange(event) {
+    onChangeSort(event.target.value);
+  }
+
+  function handleFilterChange(event) {
+    onChangeFilter(event.target.value);
+  }
+
   return (
     <div>
-      <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      <strong>Sort by:</strong>
+      <label>
+        <input
+          type="radio"
+          value="Alphabetically"
+          name="sort"
+          checked={sortBy === "Alphabetically"}
+          onChange={handleSortChange}
+        />
+        Alphabetically
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="Price"
+          name="sort"
+          checked={sortBy === "Price"}
+          onChange={handleSortChange}
+        />
+        Price
+      </label>
+      <br />
+      <label>
+        <strong>Filter:</strong>
+        <select onChange={handleFilterChange} value={filterBy}>
+          <option value="Tech">Tech</option>
+          <option value="Sportswear">Sportswear</option>
+          <option value="Finance">Finance</option>
+        </select>
+      </label>
     </div>
   );
 }
 
-export default PortfolioContainer;
+export default SearchBar;
